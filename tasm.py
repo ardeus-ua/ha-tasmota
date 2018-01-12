1e6a39ede9ff6dee669ef3d4f6f2c5ab176bb598
ardeus-ua <ardeus@gmail.com>
2018-01-12 19:30:02 +0200

    Update tasm.py

--- tasm.py
+++ tasm.py
@@ -1,8 +1,5 @@
 """
-Support for MQTT lights.
-
-For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/light.mqtt/
+Support for Tasmota WS2812 connected led strips via MQTT
 """
 import asyncio
 import logging
